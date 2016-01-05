e20af0e95b9e5a70991a24dbe52629a6b69f241c
“Sumit <er.sumitarora@gmail.com>
2016-01-05 15:14:46 -0500

    load app based on url id

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,27 +1,29 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/common_dom';
-import {ROUTER_PROVIDERS, LocationStrategy, PathLocationStrategy, APP_BASE_HREF} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy,
+    PathLocationStrategy, APP_BASE_HREF} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {provide} from 'angular2/core';
 
-// import {TodoService} from './todo-app/todo-service';
-// import {TodoApp} from './todo-app/todo-app';
-// export function main3() {
-//   return bootstrap(TodoApp, [
-//     TodoService
-//   ])
-//   .catch(err => console.error(err));
-// }
-//
-// import {App} from './app/app';
-// export function main2() {
-//   return bootstrap(App, [
-//     HTTP_PROVIDERS,
-//     ROUTER_PROVIDERS,
-//     ELEMENT_PROBE_PROVIDERS
-//   ])
-//   .catch(err => console.error(err));
-// }
+import {TodoService} from './todo-app/todo-service';
+import {TodoApp} from './todo-app/todo-app';
+export function main3() {
+  return bootstrap(TodoApp, [
+    TodoService
+  ])
+  .catch(err => console.error(err));
+}
+
+
+import {App} from './app/app';
+export function main4() {
+  return bootstrap(App, [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    ELEMENT_PROBE_PROVIDERS
+  ])
+  .catch(err => console.error(err));
+}
 
 import {RouterApp} from './router-app/router-app';
 export function main() {
@@ -33,13 +35,32 @@ export function main() {
   .catch(err => console.error(err));
 }
 
-// import {HelloApp} from './hello-app/hello-app';
-// export function main() {
-//     return bootstrap(HelloApp)
-//     .catch(err => console.error(err));
-// }
+import {HelloApp} from './hello-app/hello-app';
+export function main2() {
+    return bootstrap(HelloApp)
+    .catch(err => console.error(err));
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var appId = getParameterByName('appId');
+console.log(appId);
 
 export function init() {
-  main();
+    if (appId == '0') {
+        main();
+    } else if (appId == '1') {
+        main2();
+    } else if (appId == '2') {
+        main3();
+    } else if (appId == '3') {
+        main4();
+    }
 }
+
 document.addEventListener('DOMContentLoaded', init);
